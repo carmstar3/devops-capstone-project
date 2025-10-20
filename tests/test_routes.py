4da62041ccb3133d5ed6a5c3f862b8fc4ace8534
carmstar3 <carmstar@gmail.com>
2025-10-20 16:48:26 -0600

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -114,6 +114,13 @@ class TestAccountService(TestCase):
         response = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "*")
+      
+      def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
 
     def test_create_account(self):
         """Create: It should Create a new Account"""
